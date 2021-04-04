0b524e5ca7f684265cc4cad270da10dcf7dfb55c
abd ktefane <abd.ktefane.7@gmail.com>
2021-04-04 18:04:22 +0300

    working on structure

--- lib/features/auth/domain/repositories/auth_repositories.dart
+++ lib/features/auth/domain/repositories/auth_repositories.dart
@@ -1,7 +1,6 @@
 import 'package:core_sdk/data/repositories/base_repository.dart';
 import 'package:core_sdk/utils/network_result.dart';
 import 'package:flutter/foundation.dart';
-import 'package:mawaheb_app/base/data/models/base_response_model.dart';
 import 'package:mawaheb_app/features/auth/data/datasources/auth_datasource.dart';
 
 abstract class AuthRepository extends BaseRepository {
