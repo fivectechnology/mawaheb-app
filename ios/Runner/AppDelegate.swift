f9b954d8110e6d7c34e8bcbdd719b014eb96f1d8
Abd Al-Rahman Al-Ktefane <abd.ktefane.7@gmail.com>
2021-05-02 04:43:30 -0700

    fix flutter downloader ios crach

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,8 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import flutter_downloader
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,13 +13,10 @@ import GoogleMaps
   GMSServices.provideAPIKey("AIzaSyADYMBq9jwru-sZa5Dta6sYK9O-vZUacQI")
 
     GeneratedPluginRegistrant.register(with: self)
-    FlutterDownloaderPlugin.setPluginRegistrantCallback(registerPlugins)
+    
+    FlutterDownloaderPlugin.setPluginRegistrantCallback({ registry in if (!registry.hasPlugin("FlutterDownloaderPlugin")) { FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!) } })
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
-  private func registerPlugins(registry: FlutterPluginRegistry) { 
-    if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
-       FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!)
-    }
-}
+    
 }
